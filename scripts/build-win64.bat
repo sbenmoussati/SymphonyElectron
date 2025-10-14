36247d89b18549caabb8abb5f02bf87966f7e57d
Salah Benmoussati <salah.benmoussati@symphony.com>
2025-10-14 21:05:00 +0200

    fix

--- scripts/build-win64.bat
+++ scripts/build-win64.bat
@@ -42,15 +42,12 @@ echo SYMPHONY_EXE_PATH=%SYMPHONY_EXE_PATH%
 echo SYMPHONY_MSI_PATH=%SYMPHONY_MSI_PATH%
 echo ============================
 
-set "NPM_CACHE=C:\jenkins\.npm-cache-win"
-if not exist "%NPM_CACHE%" mkdir "%NPM_CACHE%"
-call npm config set cache "%NPM_CACHE%"
 
 :: ================================
 :: Install dependencies
 :: ================================
 echo === Installing npm dependencies ===
-call npm install --cache "%NPM_CACHE%" --prefer-offline
+call npm install
 
 :: ================================
 :: Snyk test & monitor
